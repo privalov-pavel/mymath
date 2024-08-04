8c2a2914817dd0f371ebf4b17ade62819b3ef2b3
privalov-pavel <veres.paq@gmail.com>
2024-08-04 11:34:03 +0300

    v1.0.0

--- mymath.go
+++ mymath.go
@@ -33,3 +33,15 @@ func Yn(x int, y float64) float64 {
 func Abs(x float64) float64 {
 	return math.Abs(x)
 }
+
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
+}
+
+func Asin(x float64) float64 {
+	return math.Asin(x)
+}
